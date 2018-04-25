30e22238b36a1e8de73656c8e0cf08effe3f01c5
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-04-25 22:21:57 +1000

    complete Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,20 +16,27 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+-- (<$>) :: (a -> b) -> f a -> f b
+-- (<$>) :: (a -> b) -> f (g a) -> f (g b)
+  (<$>) fab (Compose st) = Compose ((fab <$>) <$> st)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+-- pure :: a -> f (g a)
+  pure a = Compose (pure $ pure a)
+
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+-- (<*>) :: f (a -> b) -> f a -> f b
+-- (<*>) :: f (g (a -> b)) -> f (g a) -> f (g b)
+  (<*>) (Compose fgab) (Compose fga) = Compose (lift2 (<*>) fgab fga)
+
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+-- (=<<) :: (a -> f b) -> f a -> f b
+-- (=<<) :: (a -> f (g b)) -> f (g a) -> f (g b)
+  (=<<) fafb (Compose fga) = fafb <$> fga
+-- f (g (f (g b))) //no way to join
+    -- error "todo: Course.Compose (<<=)#instance (Compose f g)"
